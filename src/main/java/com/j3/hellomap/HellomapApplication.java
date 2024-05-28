72c357ce351469fd2c2cf899d320ce558ca1346f
jwelyl <cork2586@naver.com>
2024-05-28 22:41:56 +0900

    Initial commit

---
+++ src/main/java/com/j3/hellomap/HellomapApplication.java
@@ -0,0 +1,13 @@
+package com.j3.hellomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellomapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j3/hellomap/HellomapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j3.hellomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
