9f8dd5f008586ba000b4b784b8e011859956b7c9
jwelyl <cork2586@naver.com>
2024-09-19 23:23:39 +0900

    [feat] code convention, branch name convention
    [IMS] N/A

---
+++ src/main/java/com/j3/pictrails/PicTrailsApplication.java
@@ -0,0 +1,11 @@
+package com.j3.pictrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicTrailsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PicTrailsApplication.class, args);
+  }
+}
